0d712a2f22c7893d9df4649f1fc7b732058177c7
Rowan <rdschlott@gmail.com>
2025-12-22 20:03:27 -0600

    Minor fixes

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cctype>
+#include <algorithm>
+
 
 // Inserts a key, value pair into the map
 void put(std::unordered_map<std::string, std::string>& map, const std::string& key,  const std::string& value)
@@ -11,22 +14,39 @@ void put(std::unordered_map<std::string, std::string>& map, const std::string& k
 }
 
 // Retrieves a value given its key
-void get(const std::unordered_map<std::string, std::string>& map, std::string& key)
+void get(const std::unordered_map<std::string, std::string>& map, const std::string& key)
 {
-    if (map.find(key) != map.end())
+    const auto it = map.find(key);
+
+    if (it != map.end())
     {
-        std::cout << key << " => " << map.at(key) << "\n";
+        std::cout << it -> first << " => " << it -> second << "\n";
     }
     else
     {
-        std::cout << key << " not found in map\n";
+        std::cout << key << " not found in map" << std::endl;
     }
 }
 
 // Deletes a key
-void del(std::unordered_map<std::string, std::string>& map, std::string& key)
+void del(std::unordered_map<std::string, std::string>& map, const std::string& key)
 {
     map.erase(key);
+}
+
+void print_map(const std::unordered_map<std::string, std::string>& map)
+{
+    std::cout << "\n";
+    for (const auto& [k, v] : map) {
+        std::cout << k << " => " << v << "\n";
+    }
+    std::cout << "\n";
+}
+
+static std::string to_upper(std::string s)
+{
+    std::transform(s.begin(), s.end(), s.begin(),[](unsigned char c){ return std::toupper(c); });
+    return s;
 }
 
 int main ()
@@ -34,33 +54,31 @@ int main ()
 
     // Init Data Structures
     std::unordered_map<std::string, std::string> umap;
-    std::string line, command, argument;
+    std::string line, command;
 
     // While run until user types "EXIT"
     while (true){
 
-        std::vector<std::string> args;
 
         std::cout<<"Enter your command (type EXIT to quit): ";
-        std::getline(std::cin,line);
-        std::stringstream ss(line);
+        if (!std::getline(std::cin, line)) break;
+        if (line.empty()) continue;
 
+        std::stringstream ss(line);
         ss >> command;
+        command = to_upper(command);
 
         if (command == "EXIT")
         {
             break;
         }
 
+        std::vector<std::string> args;
+        std::string argument;
+
         while (ss >> argument)
         {
             args.push_back(argument);
-        }
-
-        if (args.size() == 0 || args.size() > 2)
-        {
-            std::cout<< "Invalid amount of arguments" << std::endl;
-            return 1;
         }
 
         if (command == "PUT")
@@ -68,45 +86,43 @@ int main ()
             if (args.size() != 2)
             {
                 std::cout<< "Invalid arguments, correct use: PUT <key> <value>" << std::endl;
+            } else
+            {
+                put(umap, args[0], args[1]);
             }
-            put(umap, args[0], args[1]);
 
-            // Prints Map
-            std::cout << std::endl;
-            for (const auto& [k, v] : umap) {
-                std::cout << k << " => " << v << "\n";
-            }
-            std::cout << std::endl;
-        }
+            print_map(umap);
 
-        if (command == "GET")
+        } else if (command == "GET")
         {
             if (args.size() != 1)
             {
                 std::cout<< "Invalid arguments, correct use: GET <key>" << std::endl;
+            } else
+            {
+                get(umap, args[0]);
             }
-            get(umap, argument);
 
-        }
 
-        if (command == "DEL")
+        } else if  (command == "DEL")
         {
             if (args.size() != 1)
             {
                 std::cout<< "Invalid arguments, correct use: DEL <key>" << std::endl;
             }
-            del(umap, argument);
+            else
+            {
+                del(umap, args[0]);
+            }
 
             // Prints Map
-            std::cout << std::endl;
-            for (const auto& [k, v] : umap) {
-                std::cout << k << " => " << v << "\n";
-            }
-            std::cout << std::endl;
+            print_map(umap);
 
         }
+        else
+        {
+            std:: cout << "Invalid command: " << command <<  std::endl;
+        }
     }
-
-
 }
 
